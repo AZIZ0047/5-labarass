56ef08afcfd51bf682dd7dafd9cb39b628d9040d
AZIZ0047 <xisravova@mail.ru>
2022-04-21 15:31:09 +0500

    5aya5aya5aya5aya5aya

--- main.c
+++ main.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <windows.h>
+
+
 
 int main()
 {
-    printf("Hello world!\n");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    int a, b;
+    printf("Введите два целых числа\n");
+    printf("Первое число : ");
+    scanf ("%d", &a);
+    printf("Второе число : ");
+    scanf ("%d", &b);
     return 0;
 }
